3b5298f5cd6f5645106e0ada785e9ca65f9dd5c8
pablo <paperezpablo@gmail.com>
2023-09-07 10:18:51 -0300

    funcionalidad

--- js/main.js
+++ js/main.js
@@ -2,3 +2,27 @@
 
 const btnSwitch = document.querySelector("#switch");
 
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+
+    // Se guarda el modo 
+
+    if ( document.body.classList.contains("dark")) {
+        localStorage.setItem("dark-mode", "true");
+    }else{
+        localStorage.setItem("dark-mode", "false");
+    }
+})
+
+// Lo actualizo el modo
+
+if( localStorage.getItem("dark-mode") === "true"){
+    document.body.classList.add("dark");
+    btnSwitch.classList.add("acitve");
+}else{
+    document.body.classList.remove("dark");
+}
+
+
